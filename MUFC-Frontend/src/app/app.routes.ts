f7350485776c4f58ac8124a92f8040eaa64f6dac
huybq26 <bui.quang.huy.2002@gmail.com>
2024-12-11 20:57:04 +0700

    Add home page

--- MUFC-Frontend/src/app/app.routes.ts
+++ MUFC-Frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './features/auth/login/login.component';
 import {RegisterComponent} from './features/auth/register/register.component';
+import {HomeComponent} from './features/home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-
 ];
---
+++ MUFC-Frontend/src/app/features/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
